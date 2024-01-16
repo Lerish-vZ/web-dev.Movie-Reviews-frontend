3469fc30c2ee5e35bb8eaa59bf56f084e288d7dd
Lerish-vZ <lerishavz@gmail.com>
2024-01-16 11:22:20 +0200

    Update

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,29 +1,29 @@
 import axios from "axios";
 class MovieDataService {
   getAll(page = 0) {
-    return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`);
+    return axios.get(`https://lvz-web-development-movie-reviews-app.onrender.com/api/v1/movies?page=${page}`); //http://localhost:5000/api/v1/movies?page=${page}
   }
   get(id) {
-    return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`);
+    return axios.get(`https://lvz-web-development-movie-reviews-app.onrender.com/api/v1/id/${id}`); //http://localhost:5000/api/v1/movies/id/${id}
   }
   find(query, by = "title", page = 0) {
     return axios.get(
-      `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`
-    );
+      `https://lvz-web-development-movie-reviews-app.onrender.com/api/v1/movies?${by}=${query}&page=${page}`
+    ); //http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}
   }
   createReview(data) {
-    return axios.post("http://localhost:5000/api/v1/movies/review", data);
+    return axios.post("https://lvz-web-development-movie-reviews-app.onrender.com/api/v1/movies/review", data); //http://localhost:5000/api/v1/movies/review
   }
   updateReview(data) {
-    return axios.put("http://localhost:5000/api/v1/movies/review", data);
+    return axios.put("https://lvz-web-development-movie-reviews-app.onrender.com/api/v1/movies/review", data); //http://localhost:5000/api/v1/movies/review
   }
   deleteReview(id, userId) {
-    return axios.delete("http://localhost:5000/api/v1/movies/review", {
+    return axios.delete("https://lvz-web-development-movie-reviews-app.onrender.com/api/v1/movies/review", { //http://localhost:5000/api/v1/movies/review
       data: { review_id: id, user_id: userId },
     });
   }
   getRatings() {
-    return axios.get("http://localhost:5000/api/v1/movies/ratings");
+    return axios.get("https://lvz-web-development-movie-reviews-app.onrender.com/api/v1/movies/ratings"); //http://localhost:5000/api/v1/movies/ratings
   }
 }
 export default new MovieDataService();
